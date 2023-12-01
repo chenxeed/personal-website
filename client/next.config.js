aca13fc05cd51588fcbcfcc4fc81db1b35742db2
Albert Mulia Shintra <albert.mulia@checkbox.ai>
2023-12-01 22:05:52 +0700

    (client) chore: skip ts on build

--- client/next.config.js
+++ client/next.config.js
@@ -12,10 +12,16 @@ const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    // Temporarily need this as linting in the remote server takes very long.
+    // Disable this as linting in the remote server takes very long.
     // We shall lint locally before pushing and deploying to production.
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // Disable this as building in the remote server takes very long.
+    // This shall be checked in the dev environment before it goes to production.
+    ignoreBuildErrors: true,
+  },
+  
 }
 
 module.exports = nextConfig
