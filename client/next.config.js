d6bbed2b151c92c2e67c5f640025f217ad8a43b8
Albert Mulia Shintra <albert.mulia@checkbox.ai>
2023-12-01 12:25:19 +0700

    (client) chore: skip lint on build

--- client/next.config.js
+++ client/next.config.js
@@ -9,6 +9,13 @@ const nextConfig = {
       },
     ],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    // Temporarily need this as linting in the remote server takes very long.
+    // We shall lint locally before pushing and deploying to production.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
